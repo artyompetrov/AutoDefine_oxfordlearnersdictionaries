b324f5ea09b01327c94c8eafaded3b2fae27eac5
Artem Petrov <apetrov@ptsecurity.com>
2022-07-29 21:10:11 +0700

    moved ps1 scripts

---
+++ create_symlink.ps1
@@ -0,0 +1,5 @@
+#script isn't tested
+exit
+cd C:\Users\APETROV\AppData\Roaming\Anki2\addons21
+Remove-Item 570730390
+mklink /d 570730390 C:\Users\APETROV\PycharmProjects\AutoDefine_oxfordlearnersdictionaries\AutoDefineAddon\
--- remove_pycache.ps1
+++ remove_pycache.ps1
@@ -1,2 +1,3 @@
+cd ./AutoDefineAddon
 $currentfolder = Get-Location
 Get-ChildItem -Path $currentfolder -Directory -Include __pycache__ -Recurse | Remove-Item -Force -Recurse -Verbose
